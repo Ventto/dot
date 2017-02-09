c0287e3fc9371ad732540a0bc8818f4a23c88d71
Thomas Venries <thomas.venries@gmail.com>
2017-02-09 13:20:38 +0100

    i3: Updated esthetic drasticly
    - Removed spaghetti for only one config file;
    - Changed i3 for i3-gaps;
    - Added gaps between windows;
    - Changed top bar;
    - Added bottom bar.

--- .config/i3/contrib/i3up.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh -
-
-cat $HOME/.config/i3/i3-config/* > "$HOME/.config/i3/config" && i3-msg "restart"
--- .config/i3/i3-bar/i3-bar.sh
+++ .config/i3/i3-bar/i3-bar.sh
@@ -3,4 +3,8 @@
 echo '{"version":1}'
 echo '['
 echo '[],'
-exec conky -c ~/.config/i3/i3-bar/conky.conf
+
+case "$1" in
+    top)    exec conky -c ~/.config/i3/i3-bar/conky-bar-top.conf ;;
+    bottom) exec conky -c ~/.config/i3/i3-bar/conky-bar-btm.conf ;;
+esac
