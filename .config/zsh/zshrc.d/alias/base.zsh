45912d0641c56a5f40e64450ef249a6809243af2
Thomas Venriès <thomas.venries@gmail.com>
2017-10-31 17:59:43 +0100

    term: Avoid history file with wget
    Signed-off-by: Thomas Venriès <thomas.venries@gmail.com>

--- .config/zsh/zshrc.d/alias/base.zsh
+++ .config/zsh/zshrc.d/alias/base.zsh
@@ -24,6 +24,7 @@ alias sdiff='grep -vxf'
 alias du='ncdu '
 alias vim='nvim'
 alias vimup='nvim +PluginInstall +qall'
+alias wget='wget -o /dev/null'
 
 alias i3up="$HOME/.config/i3/contrib/i3up.sh"
 alias i3log="DISPLAY=$DISPLAY i3-dump-log | less"
