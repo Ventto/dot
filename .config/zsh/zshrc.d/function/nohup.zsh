d9c89ae229b205e7ac4ec5464ee4c7bc14ebe097
Thomas Venriès <thomas.venries@gmail.com>
2017-11-16 23:41:07 +0100

    zsh: Add restriction on arg num
    Signed-off-by: Thomas Venriès <thomas.venries@gmail.com>

--- .config/zsh/zshrc.d/function/nohup.zsh
+++ .config/zsh/zshrc.d/function/nohup.zsh
@@ -1,10 +1,13 @@
 NoHup () {
-	_cmd="${1}"; shift
-	if [ -z "$(alias "${1}")" ]; then
+    [ "$#" -ne 1 ] && return 1
+
+    local _cmd="${1}"; shift
+
+    if [ -z "$(alias "${1}")" ]; then
         nohup "${_cmd}" "${@}" > /dev/null 2>&1 &
         return 0
-	fi
-	unalias "${_cmd}"
+    fi
+    unalias "${_cmd}"
     nohup "${_cmd}" "${@}" > /dev/null 2>&1 &
-	alias "${_cmd}='NoHup ${_cmd} ${@}'"
+    alias "${_cmd}='NoHup ${_cmd} ${@}'"
 }
