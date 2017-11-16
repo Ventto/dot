7b3cf92b91b1957013b984cd9b7673f228a9857b
Thomas Venriès <thomas.venries@gmail.com>
2017-11-16 23:20:03 +0100

    zsh: Remove function keyword
    Signed-off-by: Thomas Venriès <thomas.venries@gmail.com>

--- .config/zsh/zshrc.d/function/man-helper.zsh
+++ .config/zsh/zshrc.d/function/man-helper.zsh
@@ -1,4 +1,4 @@
-function vman() {
+vman() {
     file=$(basename "${1}")
     echo "$file"
     if [ "${file##*.}" = "gz" ]; then
@@ -8,6 +8,6 @@ function vman() {
     fi
 }
 
-function xman() {
+xman() {
     help2man -N -n "Description" -h -h -v -v "${1}" | groff -man -Tascii | less
 }
