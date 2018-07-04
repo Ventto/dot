fa6f4c60c0c88fb0f5c2aee007b7cbd27aa14bb6
Thomas Venriès <thomas.venries@smile.fr>
2018-07-04 19:23:21 +0200

    i3: Rename config dir
    Signed-off-by: Thomas Venriès <thomas.venries@smile.fr>

--- .config/i3-gaps/conky/i3bar.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-echo '{"version":1}'
-echo '['
-echo '[],'
-
-exec conky -c "$HOME/.config/i3-gaps/conky/bar-$1.config"
---
+++ .config/i3/conky/i3bar.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+echo '{"version":1}'
+echo '['
+echo '[],'
+
+exec conky -c "${HOME}/.config/i3/conky/bar-${1}.config"
