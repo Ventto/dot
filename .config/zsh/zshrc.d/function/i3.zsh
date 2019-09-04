e5b7f03a7ffb5ca0ba505a2651ac3290aae59677
Thomas Venriès <thomas.venries@gmail.com>
2019-09-04 14:39:06 +0200

    zsh: Use return instead of exit for source func
    Signed-off-by: Thomas Venriès <thomas.venries@gmail.com>

--- .config/zsh/zshrc.d/function/i3.zsh
+++ .config/zsh/zshrc.d/function/i3.zsh
@@ -2,7 +2,7 @@ i3bar_conky_print_config() {
     local _filepath="$1"; shift
     if [ ! -f "$_filepath" ] || [ ! -r "$_filepath" ]; then
         echo "${_filepath}: file not found" >2
-        exit 2
+        return 2
     fi
     conky -i 1 -c "$_filepath" | sed -e 's/],$/]/' | jq ''
 }
