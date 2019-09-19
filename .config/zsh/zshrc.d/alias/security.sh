a6ec9eb2bd68e2a888934ee7a5879b6c44514a68
Thomas Venriès <thomas.venries@gmail.com>
2019-09-19 13:12:47 +0200

    zsh: Update firejail app aliases

--- .config/zsh/zshrc.d/alias/security.sh
+++ .config/zsh/zshrc.d/alias/security.sh
@@ -1,11 +1,8 @@
-# Run commands with firejail
-
-alias hard-firefox='firejail --timeout=00:45:00 \
-                             --private=$(mktemp -d) \
-                             --nogroups --dns=8.8.8.8 \
-                             --dns=8.8.4.4 \
-                             --x11=xpra firefox --safe-mode \
-                                                -no-remote \
-                                                -private-window &'
-alias hard-termite='firejail --timeout=00:05:00 termite &'
-alias vlc='firejail vlc'
+alias rfirefox="firejail --private --nogroups \
+                         --timeout=00:45:00 \
+                         --dns=8.8.8.8 --dns=8.8.4.4 \
+                         --x11=xpra \
+                         firefox -no-remote -private-window"
+alias riot-desktop="firejail riot-desktop"
+alias gimp="firejail gimp"
+alias gimp="firejail smplayer"
