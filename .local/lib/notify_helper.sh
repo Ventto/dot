a6ee6e1b9d9fcdf21333bd7844410563de118793
Thomas Venriès <thomas.venries@gmail.com>
2019-10-11 09:32:42 +0200

    mako: Explicit mako dismiss in local scripts
    Dunst does not provide such dismiss feature.
    
    Signed-off-by: Thomas Venriès <thomas.venries@gmail.com>

--- .local/lib/notify_helper.sh
+++ .local/lib/notify_helper.sh
@@ -21,17 +21,6 @@ notify_percent_to_bar()
     echo "${bar}${barempty}"
 }
 
-notify_dismiss_all()
-{
-    if ps -e | grep -E ' mako$' >/dev/null 2>&1; then
-        makoctl dismiss --all && return 0
-    fi
-    if ps -e | grep -E ' dunst$' >/dev/null 2>&1; then
-        dunst dismiss --all && return 0
-    fi
-    return 1
-}
-
 notify_get_icon()
 {
     _icon_name="$1"
@@ -49,7 +38,7 @@ notify_error()
     _appname="$1"
     _msg="$2"
 
-    notify_dismiss_all && \
+    makoctl dismiss --all && \
         notify-send -a "error" \
                     -i "$(notify_get_icon "error")" \
                     "${_appname} : ${_msg}"
