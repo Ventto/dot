a8dcb58258a40a93c20cba5d7e809b8025eef5bf
Thomas Venriès <thomas.venries@gmail.com>
2019-10-12 02:19:12 +0200

    waybar: Fix indents in config and style
    Signed-off-by: Thomas Venriès <thomas.venries@gmail.com>

--- .config/waybar/config/bottom.js
+++ .config/waybar/config/bottom.js
@@ -1,70 +1,69 @@
 {
-	"layer": "top",
-	"position": "bottom",
+    "layer": "top",
+    "position": "bottom",
 
-	"modules-left": [
-		"tray",
+    "modules-left": [
+        "tray",
         "custom/right-arrow-dark"
-	],
-	"modules-center": [
-		"custom/left-arrow-dark",
-		"custom/left-arrow-light",
-		"custom/left-arrow-dark",
-		"network",
-		"custom/right-arrow-dark",
-		"custom/right-arrow-light",
-		"custom/right-arrow-dark"
-	],
-	"modules-right": [
-		"custom/left-arrow-dark",
+    ],
+    "modules-center": [
+        "custom/left-arrow-dark",
+        "custom/left-arrow-light",
+        "custom/left-arrow-dark",
+        "network",
+        "custom/right-arrow-dark",
+        "custom/right-arrow-light",
+        "custom/right-arrow-dark"
+    ],
+    "modules-right": [
+        "custom/left-arrow-dark",
         "custom/bluetooth-devices",
-		"custom/left-arrow-light",
-
-		"custom/left-arrow-dark",
-        "custom/storage-devices",
-		"custom/left-arrow-light",
-
-		"custom/left-arrow-dark",
-        "custom/serial-devices",
-		"custom/left-arrow-light",
+        "custom/left-arrow-light",
 
         "custom/left-arrow-dark",
-		"custom/disk-root",
-		"custom/left-arrow-light",
+        "custom/storage-devices",
+        "custom/left-arrow-light",
 
-		"custom/left-arrow-dark",
-		"custom/disk-home",
-		"custom/left-arrow-light",
+        "custom/left-arrow-dark",
+        "custom/serial-devices",
+        "custom/left-arrow-light",
 
-		"custom/left-arrow-dark"
-	],
+        "custom/left-arrow-dark",
+        "custom/disk-root",
+        "custom/left-arrow-light",
 
-	"custom/left-arrow-dark": {
-		"format": "",
-		"tooltip": false
-	},
-	"custom/left-arrow-light": {
-		"format": "",
-		"tooltip": false
-	},
-	"custom/right-arrow-dark": {
-		"format": "",
-		"tooltip": false
-	},
-	"custom/right-arrow-light": {
-		"format": "",
-		"tooltip": false
-	},
-	"custom/disk-home": {
-		"format": "home {} ",
-		"interval": 30,
-		"exec": "df -h --output=avail $HOME | tail -1 | tr -d ' '"
-	},
-	"custom/disk-root": {
-		"format": "root {} ",
-		"interval": 30,
-		"exec": "df -h --output=avail / | tail -1 | tr -d ' '"
-	},
+        "custom/left-arrow-dark",
+        "custom/disk-home",
+        "custom/left-arrow-light",
+
+        "custom/left-arrow-dark"
+    ],
+    "custom/left-arrow-dark": {
+        "format": "",
+        "tooltip": false
+    },
+    "custom/left-arrow-light": {
+        "format": "",
+        "tooltip": false
+    },
+    "custom/right-arrow-dark": {
+        "format": "",
+        "tooltip": false
+    },
+    "custom/right-arrow-light": {
+        "format": "",
+        "tooltip": false
+    },
+    "custom/disk-home": {
+        "format": "home {} ",
+        "interval": 30,
+        "exec": "df -h --output=avail $HOME | tail -1 | tr -d ' '"
+    },
+    "custom/disk-root": {
+        "format": "root {} ",
+        "interval": 30,
+        "exec": "df -h --output=avail / | tail -1 | tr -d ' '"
+    },
     "custom/bluetooth-devices": {
         "format": "{}",
         "exec": "${HOME}/.config/waybar/modules/bluetooth/exec.sh",
@@ -82,13 +81,13 @@
         "exec": "${HOME}/.config/waybar/modules/storage.sh",
         "interval": 5
     },
-	"network": {
-		"format-wifi": "<span color='#589df6'></span> <span color='gray'>{essid}</span> <span color='#589df6'>{signalStrength}%</span> {ipaddr}/{cidr}",
-		"format-ethernet": " {ifname}: {ipaddr}/{cidr}",
-		"format-linked": " {ifname} (No IP)",
-		"format-disconnected": "Disconnected ",
-		"format-alt": "{ifname}: {ipaddr}/{cidr}",
-		"interval": 3,
-		"tooltip": false
-	}
+    "network": {
+        "format-wifi": "<span color='#589df6'></span> <span color='gray'>{essid}</span> <span color='#589df6'>{signalStrength}%</span> {ipaddr}/{cidr}",
+        "format-ethernet": " {ifname}: {ipaddr}/{cidr}",
+        "format-linked": " {ifname} (No IP)",
+        "format-disconnected": "Disconnected ",
+        "format-alt": "{ifname}: {ipaddr}/{cidr}",
+        "interval": 3,
+        "tooltip": false
+    }
 }
--- .config/waybar/config/top.js
+++ .config/waybar/config/top.js
@@ -1,133 +1,133 @@
 {
-	"layer": "top",
-	"position": "top",
+    "layer": "top",
+    "position": "top",
 
-	"modules-left": [
-		"sway/workspaces",
-		"custom/right-arrow-dark"
-	],
-	"modules-center": [
-		"custom/left-arrow-dark",
-		"custom/left-arrow-light",
+    "modules-left": [
+        "sway/workspaces",
+        "custom/right-arrow-dark"
+    ],
+    "modules-center": [
+        "custom/left-arrow-dark",
+        "custom/left-arrow-light",
 
-		"custom/left-arrow-dark",
-		"clock#1",
-		"custom/left-arrow-light",
+        "custom/left-arrow-dark",
+        "clock#1",
+        "custom/left-arrow-light",
 
-		"custom/left-arrow-dark",
-		"clock#2",
-		"custom/right-arrow-dark",
+        "custom/left-arrow-dark",
+        "clock#2",
+        "custom/right-arrow-dark",
 
-		"custom/right-arrow-light",
-		"clock#3",
-		"custom/right-arrow-dark",
+        "custom/right-arrow-light",
+        "clock#3",
+        "custom/right-arrow-dark",
 
-		"custom/right-arrow-light",
-		"custom/right-arrow-dark"
-	],
-	"modules-right": [
-		"custom/left-arrow-dark",
-		"temperature",
-		"cpu",
-		"custom/left-arrow-light",
+        "custom/right-arrow-light",
+        "custom/right-arrow-dark"
+    ],
+    "modules-right": [
+        "custom/left-arrow-dark",
+        "temperature",
+        "cpu",
+        "custom/left-arrow-light",
 
-		"custom/left-arrow-dark",
-		"memory",
-		"custom/left-arrow-light",
+        "custom/left-arrow-dark",
+        "memory",
+        "custom/left-arrow-light",
 
-		"custom/left-arrow-dark",
-		"backlight",
-		"custom/left-arrow-light",
+        "custom/left-arrow-dark",
+        "backlight",
+        "custom/left-arrow-light",
 
-		"custom/left-arrow-dark",
-		"pulseaudio",
-		"custom/left-arrow-light",
+        "custom/left-arrow-dark",
+        "pulseaudio",
+        "custom/left-arrow-light",
 
-		"custom/left-arrow-dark",
-		"battery",
-		"custom/left-arrow-light",
+        "custom/left-arrow-dark",
+        "battery",
+        "custom/left-arrow-light",
 
-		"custom/left-arrow-dark"
-	],
+        "custom/left-arrow-dark"
+    ],
 
-	"custom/left-arrow-dark": {
-		"format": "",
-		"tooltip": false
-	},
-	"custom/left-arrow-light": {
-		"format": "",
-		"tooltip": false
-	},
-	"custom/right-arrow-dark": {
-		"format": "",
-		"tooltip": false
-	},
-	"custom/right-arrow-light": {
-		"format": "",
-		"tooltip": false
-	},
+    "custom/left-arrow-dark": {
+        "format": "",
+        "tooltip": false
+    },
+    "custom/left-arrow-light": {
+        "format": "",
+        "tooltip": false
+    },
+    "custom/right-arrow-dark": {
+        "format": "",
+        "tooltip": false
+    },
+    "custom/right-arrow-light": {
+        "format": "",
+        "tooltip": false
+    },
 
-	"sway/workspaces": {
-		"disable-scroll": true,
-		"format": "{name}"
-	},
+    "sway/workspaces": {
+        "disable-scroll": true,
+        "format": "{name}"
+    },
 
-	"clock#1": {
-		"format": "{:%A}",
-		"tooltip": false
-	},
-	"clock#3": {
-		"format": "{:%H:%M}",
-		"tooltip": false
-	},
-	"clock#2": {
-		"format": "{:%d %b}",
-		"tooltip": false
-	},
+    "clock#1": {
+        "format": "{:%A}",
+        "tooltip": false
+    },
+    "clock#3": {
+        "format": "{:%H:%M}",
+        "tooltip": false
+    },
+    "clock#2": {
+        "format": "{:%d %b}",
+        "tooltip": false
+    },
     "backlight": {
         "format": "{icon} {percent}%",
         "format-alt": "{percent}% {icon}",
         "format-alt-click": "click-right",
         "format-icons": [ "" ]
     },
-	"pulseaudio": {
-		"format": "{icon} {volume}%",
-		"format-bluetooth": "{icon} {volume}%",
-		"format-muted": " x",
-		"format-icons": {
-			"headphones": "",
-			"default": [ "", "" ]
-		},
-		"scroll-step": 1,
-		"on-click": "pavucontrol"
-	},
-	"memory": {
-		"interval": 2,
-		"format": " {}%",
-		"tooltip": true
-	},
-    "temperature": {
-	    "critical-threshold": 80,
-	    "format-critical": "{temperatureC}°C ",
-	    "format": "{temperatureC}°C "
+    "pulseaudio": {
+        "format": "{icon} {volume}%",
+        "format-bluetooth": "{icon} {volume}%",
+        "format-muted": " x",
+        "format-icons": {
+            "headphones": "",
+            "default": [ "", "" ]
+        },
+        "scroll-step": 1,
+        "on-click": "pavucontrol"
     },
-	"cpu": {
-		"interval": 2,
-		"format": " {usage}%",
+    "memory": {
+        "interval": 2,
+        "format": " {}%",
+        "tooltip": true
+    },
+    "temperature": {
+        "critical-threshold": 80,
+        "format-critical": "{temperatureC}°C ",
+        "format": "{temperatureC}°C "
+    },
+    "cpu": {
+        "interval": 2,
+        "format": " {usage}%",
         "max-length": 10
-	},
-	"battery": {
-		"interval": 2,
-		"states": {
-			"good": 95,
-			"warning": 30,
-			"critical": 15
-		},
-		"format": "{icon} {capacity}%",
-		"format-charging": "+ {capacity}%",
-		"format-icons": [ "", "", "", "", "" ]
-	},
-	"tray": {
-		"icon-size": 20
-	}
+    },
+    "battery": {
+        "interval": 2,
+        "states": {
+            "good": 95,
+            "warning": 30,
+            "critical": 15
+        },
+        "format": "{icon} {capacity}%",
+        "format-charging": "+ {capacity}%",
+        "format-icons": [ "", "", "", "", "" ]
+    },
+    "tray": {
+        "icon-size": 20
+    }
 }
