7ad9c3462ade8c2574c3d39b189262df27552be1
Thomas Venriès <thomas.venries@gmail.com>
2020-08-02 17:13:43 +0200

    zsh: Update qt env var prefix for virtualbox

--- .config/zsh/zshrc.d/alias/apps.sh
+++ .config/zsh/zshrc.d/alias/apps.sh
@@ -5,7 +5,6 @@ alias i3log="DISPLAY=${DISPLAY} i3-dump-log | less"
 alias wanip='curl -s http://ip.tyk.nu/'
 alias weather="curl -s wttr.in/Paris"
 alias weather-now="curl -s wttr.in/Paris | sed -n '3,7p'"
-
 alias pickcolor="grim -g \"\$(slurp -p)\" -t ppm - \
                     | convert - -format '%[pixel:p{0,0}]' txt:-"
 alias dolphin-here="QT_QPA_PLATFORMTHEME=qt5ct nohup dolphin . >/dev/null 2>&1 &"
@@ -13,5 +12,5 @@ alias pdfviewer="okular"
 alias imgviewer="nomacs"
 alias imgeditor="gimp"
 alias printer-manager="system-config-printer"
-alias VirtualBox="QT_QPA_PLATFORM=xcb QT_FONT_DPI=96 VirtualBox"
-alias virtualbox="QT_QPA_PLATFORM=xcb QT_FONT_DPI=96 VirtualBox"
+alias VirtualBox="QT_QPA_PLATFORM=xcb VirtualBox"
+alias virtualbox="QT_QPA_PLATFORM=xcb VirtualBox"
