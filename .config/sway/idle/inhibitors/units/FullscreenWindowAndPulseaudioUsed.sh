15a397fa21343113c93e3fc6f32eeb1923ca3e3d
Thomas Venriès <me@ventto.io>
2021-03-07 15:00:00 +0100

    swayidle: use pid instead of unreliable app name
    The pacmd command gives application name and binary name that sometimes
    does not match the app_id or class from the `swaymsg -t get_tree`
    command.

--- .config/sway/idle/inhibitors/units/FullscreenWindowAndPulseaudioUsed.sh
+++ .config/sway/idle/inhibitors/units/FullscreenWindowAndPulseaudioUsed.sh
@@ -3,27 +3,27 @@
 #######################################
 # Checks if an application has one of its windows in fullscreen mode
 # ARGUMENTS:
-#   $1: application binary name
+#   $1: application PID
 # RETURN:
 #   0 on succeeds, non-zero on error.
 #######################################
 app_has_fullscreen_window()
 {
-    app="$1"
+    pid="$1"
     swaymsg -t get_tree | \
-        grep -Ei "\"(app_id|class)\": \"${app}\"," -B 30 |\
+        grep -Ei "\"pid\": ${pid}," -B 20 |\
         grep -E '"fullscreen_mode": [01],' | tail -n1 |\
         grep -E '"fullscreen_mode": 1,' >/dev/null 2>&1
 }
 
 #######################################
-# Prints the binary names of any application which is using PulseAudio
+# Prints the PID of any application which is using PulseAudio
 # OUTPUTS:
 #   Write application's binary names in column to stdout
 # RETURN:
 #   Always succeeds.
 #######################################
-pulseaudio_list_running_sink_input_app_process_binary_names()
+pulseaudio_list_running_sink_input_app_pids()
 {
     pacmd_output="$(pacmd list-sink-inputs)"
     input_list="$(echo "$pacmd_output" | grep -E '    index: [0-9]+')"
@@ -37,7 +37,7 @@ pulseaudio_list_running_sink_input_app_process_binary_names()
         if ! echo "$input_data" | grep 'state: RUNNING' >/dev/null 2>&1; then
             continue
         fi
-        echo "$input_data" | sed -n 's/.*application.process.binary = "\(.*\)"/\1/p'
+        echo "$input_data" | sed -n 's/.*application.process.id = "\(.*\)"/\1/p'
     done
 }
 
@@ -48,10 +48,9 @@ pulseaudio_list_running_sink_input_app_process_binary_names()
 #######################################
 is_watching_movie()
 {
-    app_names="$(pulseaudio_list_running_sink_input_app_process_binary_names)"
-    set -x
-    for name in $app_names; do
-        if app_has_fullscreen_window "$name"; then
+    app_pids="$(pulseaudio_list_running_sink_input_app_pids)"
+    for pid in $app_pids; do
+        if app_has_fullscreen_window "$pid"; then
             return 0
         fi
     done
