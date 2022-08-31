3defd996954ee9b3194e9ec5a8fea61892910273
Thomas Venriès <thomas.venries@gmail.com>
2022-08-31 23:24:43 +0200

    (backlight/volume)ctl: fix with shell google coding style

--- .local/lib/notify_helper.sh
+++ .local/lib/notify_helper.sh
@@ -5,7 +5,7 @@ notify_print_bar()
     for i in $(seq "$2"); do printf '%s' "$1"; done
 }
 
-notify_percent_to_bar()
+notify_percent_to_summary()
 {
     if [ "$#" -ne 1 ] || ! echo "$1" | grep -E '^[0-9]+$' >/dev/null 2>&1; then
         echo ""
@@ -27,19 +27,27 @@ notify_get_icon()
 
     icons_dir="/usr/lib/node_modules/material-design-icons/"
     icon_path="$(find "$icons_dir" -type f \
-                      -name "ic_*${_icon_name}_black_48dp.png" | \
+                      -name "ic_*${_icon_name}_white_48dp.png" | \
                  head -n1)"
 
     echo "$icon_path"
 }
 
-notify_error()
+notify_err()
 {
-    _appname="$1"
-    _msg="$2"
+    _subject="$1"
+    _summary="$2"
 
-    makoctl dismiss --all && \
-        notify-send -a "error" \
-                    -i "$(notify_get_icon "error")" \
-                    "${_appname} : ${_msg}"
+   notify-send -a "error" -i "$(notify_get_icon "error")" \
+       "${_subject} : ${_summary}"
+}
+
+notify_log()
+{
+    _subject="$1"
+    _icon="$2"
+    _summary="$3"
+
+    notify-send -a "$_subject" -i "$(notify_get_icon "$_icon")" \
+        "$_summary"
 }
