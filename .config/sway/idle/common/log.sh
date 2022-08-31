fd3a73242cee9f306b71adbcb154753b764f0a76
Thomas Venriès <thomas.venries@gmail.com>
2022-08-31 17:50:12 +0200

    sway: replace inhibit-idle scripts with inhibit_idle property

--- .config/sway/idle/common/log.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-prog="$(basename "$(dirname "$0")")/$(basename "$0")"
-
-log()
-{
-    printf "%s: %s: %s\n" "$(date)" "$prog" "$@"
-}
--- .config/sway/idle/inhibitors/units/PowerSupplyAdapterUsed.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-test "$(cat /sys/class/power_supply/AC/online)" = "1"
