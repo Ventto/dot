ee9a26c935427875e46933574cd767bb1f7a8208
Thomas Venriès <thomas.venries@gmail.com>
2023-02-23 22:14:43 +0100

    refactor(firefox): del specific addon icon placement

--- .config/firefox/user.js
+++ .config/firefox/user.js
@@ -4,7 +4,6 @@ user_pref("browser.tabs.inTitlebar", 0);
 user_pref("browser.theme.content-theme", 0);
 user_pref("browser.theme.toolbar-theme", 0);
 user_pref("browser.startup.homepage", "about:blank");
-user_pref("browser.uiCustomization.state", "{\"placements\":{\"widget-overflow-fixed-list\":[],\"nav-bar\":[\"urlbar-container\",\"downloads-button\",\"_testpilot-containers-browser-action\",\"https-everywhere_eff_org-browser-action\",\"cookieautodelete_kennydo_com-browser-action\",\"ublock0_raymondhill_net-browser-action\",\"tab-session-manager_sienori-browser-action\",\"keepassxc-browser_keepassxc_org-browser-action\",\"fxa-toolbar-menu-button\"],\"toolbar-menubar\":[\"menubar-items\"],\"TabsToolbar\":[\"tabbrowser-tabs\",\"new-tab-button\",\"alltabs-button\"],\"PersonalToolbar\":[\"personal-bookmarks\"]},\"seen\":[\"save-to-pocket-button\",\"developer-button\",\"https-everywhere_eff_org-browser-action\",\"cookieautodelete_kennydo_com-browser-action\",\"ublock0_raymondhill_net-browser-action\",\"enhancerforyoutube_maximerf_addons_mozilla_org-browser-action\",\"_testpilot-containers-browser-action\",\"keepassxc-browser_keepassxc_org-browser-action\",\"tab-session-manager_sienori-browser-action\"],\"dirtyAreaCache\":[\"nav-bar\",\"toolbar-menubar\",\"TabsToolbar\",\"PersonalToolbar\"],\"currentVersion\":17,\"newElementCount\":6}");
 
 user_pref("mousewheel.with_control.action", 1);
 
