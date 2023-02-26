56f99d31facf8b409e7f73988b71e364f1f2e336
Thomas Venriès <thomas.venries@gmail.com>
2023-02-26 23:42:17 +0100

    refactor(firefox): same font for ui

--- .config/firefox/user.js
+++ .config/firefox/user.js
@@ -4,7 +4,5 @@ user_pref("browser.tabs.inTitlebar", 0);
 user_pref("browser.theme.content-theme", 0);
 user_pref("browser.theme.toolbar-theme", 0);
 user_pref("browser.startup.homepage", "about:blank");
-
 user_pref("mousewheel.with_control.action", 1);
-
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
