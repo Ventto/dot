67d058fe44694ab2baad709bb3995fbfa6cdf177
Thomas Venriès <thomas.venries@gmail.com>
2023-08-04 21:56:15 -0300

    firefox: add Makefile to auto-update user.js
    We use Arkenfox scripts + our overrides.

--- .config/firefox/user.js
+++
@@ -1,8 +0,0 @@
-user_pref("browser.bookmarks.showMobileBookmarks", true);
-user_pref("browser.download.autohideButton", false);
-user_pref("browser.tabs.inTitlebar", 0);
-user_pref("browser.theme.content-theme", 0);
-user_pref("browser.theme.toolbar-theme", 0);
-user_pref("browser.startup.homepage", "about:blank");
-user_pref("mousewheel.with_control.action", 1);
-user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
